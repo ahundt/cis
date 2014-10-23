5a43f62fb1340ad4a0fde04be1cb27a11625d83a
Andrew Hundt <ATHundt@gmail.com>
2014-10-23 17:25:38 -0400

    Basic command line parameter reading works

--- src/cisHW1main.cpp
+++ src/cisHW1main.cpp
@@ -16,10 +16,35 @@
 
 namespace po = boost::program_options;
 
-struct AlgorithmParams {
-	std::string inputPointCloudFile1;
-	std::string inputPointCloudFile2;
+struct AlgorithmData {
+	csvCIS_pointCloudData calbody;
+	csvCIS_pointCloudData calreadings;
+	csvCIS_pointCloudData empivot;
+	csvCIS_pointCloudData optPivot;
+	csvCIS_pointCloudData output1;
 };
+
+struct AlgorithmParams {
+	std::string  calbodyPath;
+	std::string  calreadingsPath;
+	std::string  empivotPath;
+	std::string  optpivotPath;
+	std::string  output1Path;
+};
+
+void assmblePathIfFullPathNotSupplied(std::string dataFolderPath, std::string dataFilenamePrefix, std::string suffix, std::string& dataFilePath){
+	
+	if(boost::filesystem::exists(dataFilePath)) return;
+	
+	if (dataFilePath.empty() || !boost::filesystem::exists(dataFilePath) ) {
+		dataFilePath = dataFolderPath+dataFilenamePrefix+suffix;
+    }
+	
+	if (dataFilePath.empty() || !boost::filesystem::exists(dataFilePath) ) {
+      throw std::runtime_error("File "+dataFilePath + " does not exist!");
+    }
+	
+}
 
 bool readCommandLine(int argc, char* argv[], AlgorithmParams & algorithmParams){
 
@@ -37,10 +62,21 @@ bool readCommandLine(int argc, char* argv[], AlgorithmParams & algorithmParams){
 	
 
     po::options_description algorithmOptions("Algorithm Options");
+    std::string currentPath(boost::filesystem::path( boost::filesystem::current_path() ).string());
   
     algorithmOptions.add_options()
-		  ("inputPointCloudFile",po::value<std::string>(),"PointCloudFile.txt")
-		  ("inputPointCloudFile2",po::value<std::string>(),"PointCloudFile2")
+		    ("dataFolderPath"                ,po::value<std::string>()->default_value(currentPath)       ,"folder containing data files, defaults to current working directory"   ) 
+		  	("dataFilenamePrefix"            ,po::value<std::string>()->default_value("pa1-debug-a"     ),"constant prefix of data filename path"   )
+		  	("dataFileNameSuffix_calbody"    ,po::value<std::string>()->default_value("-calbody.txt"    ),"suffix of data filename path"   )
+		  	("dataFileNameSuffix_calreadings",po::value<std::string>()->default_value("-calreadings.txt"),"suffix of data filename path"   )
+		  	("dataFileNameSuffix_empivot"    ,po::value<std::string>()->default_value("-empivot.txt"    ),"suffix of data filename path"   )
+		  	("dataFileNameSuffix_optpivot"   ,po::value<std::string>()->default_value("-optpivot.txt"   ),"suffix of data filename path"   )
+		  	("dataFileNameSuffix_output1"    ,po::value<std::string>()->default_value("-output1.txt"    ),"suffix of data filename path"   )
+		  	("calbodyPath"    ,po::value<std::string>() )
+		  	("calreadingsPath",po::value<std::string>() )
+		  	("empivotPath"    ,po::value<std::string>() )
+		  	("optpivotPath"   ,po::value<std::string>() )
+		  	("output1Path"    ,po::value<std::string>() )
 			  ;
 
     po::options_description allOptions;
@@ -70,11 +106,35 @@ bool readCommandLine(int argc, char* argv[], AlgorithmParams & algorithmParams){
   
 	parseResponseFiles(vmap,allOptions);
 	
-	po::readOption(vmap,"inputPointCloudFile",algorithmParams.inputPointCloudFile1,optional);
+	std::string  dataFolderPath
+				,dataFilenamePrefix
+				,dataFileNameSuffix_calbody 
+				,dataFileNameSuffix_calreadings
+				,dataFileNameSuffix_empivot    
+				,dataFileNameSuffix_optpivot   
+				,dataFileNameSuffix_output1;   
+		
+	po::readOption(vmap,"dataFolderPath",dataFolderPath,optional);
+	po::readOption(vmap,"dataFilenamePrefix",dataFilenamePrefix,optional);
+	po::readOption(vmap, "dataFileNameSuffix_calbody"      ,dataFileNameSuffix_calbody         ,optional);
+	po::readOption(vmap, "dataFileNameSuffix_calreadings"  ,dataFileNameSuffix_calreadings     ,optional);
+	po::readOption(vmap, "dataFileNameSuffix_empivot"      ,dataFileNameSuffix_empivot         ,optional);
+	po::readOption(vmap, "dataFileNameSuffix_optpivot"     ,dataFileNameSuffix_optpivot        ,optional);
+	po::readOption(vmap, "dataFileNameSuffix_output1"      ,dataFileNameSuffix_output1         ,optional);
+	                                                                                           
+    
+    po::readOption(vmap,"calbodyPath",algorithmParams.calbodyPath,optional);
+    po::readOption(vmap,"calreadingsPath",algorithmParams.calreadingsPath,optional);
+	po::readOption(vmap,"empivotPath",algorithmParams.empivotPath,optional);
+	po::readOption(vmap,"optpivotPath",algorithmParams.optpivotPath,optional);
+	po::readOption(vmap,"output1Path",algorithmParams.output1Path,optional);
 	
-    if (!algorithmParams.inputPointCloudFile1.empty() && !boost::filesystem::exists(algorithmParams.inputPointCloudFile1) ) {
-      throw std::runtime_error("File "+algorithmParams.inputPointCloudFile1 + " does not exist!");
-    }
+	assmblePathIfFullPathNotSupplied(dataFolderPath,dataFilenamePrefix,dataFileNameSuffix_calbody       ,algorithmParams.calbodyPath);
+	assmblePathIfFullPathNotSupplied(dataFolderPath,dataFilenamePrefix,dataFileNameSuffix_calreadings   ,algorithmParams.calreadingsPath);
+	assmblePathIfFullPathNotSupplied(dataFolderPath,dataFilenamePrefix,dataFileNameSuffix_empivot       ,algorithmParams.empivotPath);
+	assmblePathIfFullPathNotSupplied(dataFolderPath,dataFilenamePrefix,dataFileNameSuffix_optpivot      ,algorithmParams.optpivotPath);
+	assmblePathIfFullPathNotSupplied(dataFolderPath,dataFilenamePrefix,dataFileNameSuffix_output1       ,algorithmParams.output1Path);
+	
 	
     return false;
 }
@@ -95,7 +155,7 @@ int main(int argc,char**argv) {
 	readCommandLine(argc,argv,ap);
 	
 	std::stringstream ss;
-	ss << std::ifstream( ap.inputPointCloudFile1.c_str() ).rdbuf();
+	ss << std::ifstream( ap.calbodyPath ).rdbuf();
     
 	csvCIS_pointCloudData firstPointCloud = parseCSV_CIS_pointCloud(ss.str(),true);
 	

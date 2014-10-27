31ad12cee7062a5df4ddb7ed09cdba0f6b56ec38
astrick3 <astrick3@jhu.edu>
2014-10-27 15:19:50 -0400

    Added functions to pivot_calibration header

--- include/PivotCalibration.hpp
+++ include/PivotCalibration.hpp
@@ -1,4 +1,27 @@
 #ifndef _PIVOT_CALIBRATION_HPP_
 #define _PIVOT_CALIBRATION_HPP_
 
+Eigen::MatrixXd makeAmatrix(Eigen::MatrixXd F)
+{
+    Eigen::MatrixXd A(3,6);
+    A.block<3,3>(0,0) = F.block<3,3>(0,0);
+    A.block<3,3>(0,3) = -Eigen::Matrix3d::Identity();
+    return A;
+}
+
+Eigen::MatrixXd makePVector(Eigen::MatrixXd F)
+{
+    Eigen::MatrixXd P(3,1);
+    P.block<3,1>(0,0) = F.block<3,1>(0,3);
+    return P;
+}
+
+Eigen::VectorXd SVDSolve(Eigen::MatrixXd RI, Eigen::VectorXd p)
+{
+    Eigen::JacobiSVD<Eigen::MatrixXd> svd(RI, Eigen::ComputeThinU | Eigen::ComputeThinV);
+    Eigen::VectorXd X = svd.solve(-p);
+    return X;
+}
+
+
 #endif // _PIVOT_CALIBRATION_HPP_

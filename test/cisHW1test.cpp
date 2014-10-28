66ed710d2eefdad8eb7173b9147521af962910cf
astrick3 <astrick3@jhu.edu>
2014-10-27 22:40:25 -0400

    Added initial unit test for pivot calibration

--- test/cisHW1test.cpp
+++ test/cisHW1test.cpp
@@ -33,16 +33,16 @@ int add(int i, int j)
 
 template<typename T>
 void PrintTwo(T& tp1, T& tp2, std::string p1 = std::string("p1:"), std::string p2 = std::string("p2:")){
-    
+
     std::cout << "\n\n"<< p1 <<"\n\n" << tp1 << "\n\n"<< p2 <<"\n\n" << tp2 <<"\n\n";
 }
 
 struct checkHornRegistrationInverses{
     void operator()(const Eigen::MatrixXd& trackerPerspective1, const Eigen::MatrixXd& trackerPerspective2, std::string p1 = std::string("p1:"), std::string p2 = std::string("p2:")){
-        
+
         Eigen::Matrix4d F = hornRegistration(trackerPerspective1,trackerPerspective2);
         Eigen::Matrix4d Finv = hornRegistration(trackerPerspective2,trackerPerspective1);
-    
+
         BOOST_CHECK(Eigen::Matrix4d::Identity().isApprox(F*Finv,tolerance));
         if(!Eigen::Matrix4d::Identity().isApprox(F*Finv,tolerance)){
             std::cout << "\n\n"<< p1 <<"\n\n" << trackerPerspective1 << "\n\n"<< p2 <<"\n\n" << trackerPerspective2  << "\n\nF:\n\n" << F << "\n\nFinv:\n\n" << Finv << "\n\nF*Finv:\n\n" << F*Finv <<"\n\n";
@@ -53,7 +53,7 @@ struct checkHornRegistrationInverses{
 
 template<typename Visitor>
 void visitEachTracker(const csvCIS_pointCloudData::TrackerFrames& tf1,const csvCIS_pointCloudData::TrackerFrames& tf2,Visitor v){
-    
+
     auto frameIt1 = tf1.begin();
     auto frameIt2 = tf2.begin();
     for(; frameIt1 != tf1.end() && frameIt2 != tf2.end(); ++frameIt1, ++frameIt2)
@@ -65,14 +65,14 @@ void visitEachTracker(const csvCIS_pointCloudData::TrackerFrames& tf1,const csvC
             v(*trackerIt3,*trackerIt4);
         }
     }
-    
+
 }
 
 
 /// useful for going over calbody and calreadings, where there is one instance of calbody and many instances of calreadings
 template<typename Visitor>
 void visitSecondTrackerRepeatedly(const csvCIS_pointCloudData::TrackerFrames& tf1,const csvCIS_pointCloudData::TrackerFrames& tf2,Visitor v){
-    
+
     for(auto frameIt1 = tf1.begin(); frameIt1 != tf1.end(); ++frameIt1)
     {
         for(auto frameIt2 = tf2.begin(); frameIt2 != tf2.end(); ++frameIt2)
@@ -85,7 +85,7 @@ void visitSecondTrackerRepeatedly(const csvCIS_pointCloudData::TrackerFrames& tf
             }
         }
     }
-    
+
 }
 
 BOOST_AUTO_TEST_SUITE(VariantsSuite)
@@ -97,22 +97,22 @@ BOOST_AUTO_TEST_CASE(simplePass)
 BOOST_AUTO_TEST_CASE(HMatrixGMatrix)
 {
     // Test HMatrix and GMatrix functions
-    
+
     // test 1
-    
+
     Eigen::MatrixXd a(1,3);
     a << 1,0,0;
-    
+
     Eigen::MatrixXd b(1,3);
     b << 1,0,0;
-    
+
     Eigen::Matrix3d H = Hmatrix(a,b);
     Eigen::Matrix3d Hexpected;
     Hexpected <<
     1,0,0,
     0,0,0,
     0,0,0;
-    
+
     Eigen::Matrix4d G = Gmatrix(H);
     Eigen::Matrix4d Gexpected;
     Gexpected <<
@@ -120,14 +120,14 @@ BOOST_AUTO_TEST_CASE(HMatrixGMatrix)
     0,  1,  0 ,  0,
     0,  0, -1 ,  0,
     0,  0,  0 , -1;
-    
+
     BOOST_CHECK_EQUAL(Hexpected, H);
     BOOST_CHECK_EQUAL(Gexpected, G);
-    
-    
+
+
     // test 2
-    
-    
+
+
     a << 1,1,1;
     b << 1,1,1;
     H = Hmatrix(a,b);
@@ -135,21 +135,21 @@ BOOST_AUTO_TEST_CASE(HMatrixGMatrix)
     1,1,1,
     1,1,1,
     1,1,1;
-    
+
     G = Gmatrix(H);
     Gexpected <<
     3,  0,  0 ,  0,
     0, -1,  2 ,  2,
     0,  2, -1 ,  2,
     0,  2,  2 , -1;
-    
+
     BOOST_CHECK_EQUAL(Hexpected, H);
     BOOST_CHECK_EQUAL(Gexpected, G);
-    
-    
-    
+
+
+
     // test 3
-    
+
     a << 1,0,2;
     b << 1,0,2;
     H = Hmatrix(a,b);
@@ -157,41 +157,41 @@ BOOST_AUTO_TEST_CASE(HMatrixGMatrix)
     1,0,2,
     0,0,0,
     2,0,4;
-    
-    
+
+
     G = Gmatrix(H);
     Gexpected <<
     5,  0,  0 ,  0,
     0, -3,  0 ,  4,
     0,  0, -5 ,  0,
     0,  4,  0 ,  3;
-    
+
     BOOST_CHECK_EQUAL(Hexpected, H);
     BOOST_CHECK_EQUAL(Gexpected, G);
-    
-    
+
+
     // test 4
-    
+
     a.resize(2,3);
     a << 1,0,2,
          1,1,1;
     b.resize(2, 3);
     b << 1,0,2,
          1,1,1;
-    
+
     H = Hmatrix(a,b);
     Hexpected <<
     2,1,3,
     1,1,1,
     3,1,5;
-    
+
     G = Gmatrix(H);
     Gexpected <<
     8,  0,  0 ,  0,
     0, -4,  2 ,  6,
     0,  2, -6 ,  2,
     0,  6,  2 ,  2;
-    
+
     BOOST_CHECK_EQUAL(Hexpected, H);
     BOOST_CHECK_EQUAL(Gexpected, G);
 
@@ -200,7 +200,7 @@ BOOST_AUTO_TEST_CASE(HMatrixGMatrix)
 BOOST_AUTO_TEST_CASE(manualHornRegistration)
 {
     static const bool debug = false;
-    
+
     Eigen::MatrixXd trackerPoints(8,3);
 	trackerPoints <<
   		  0,   0,   0,
@@ -211,31 +211,31 @@ BOOST_AUTO_TEST_CASE(manualHornRegistration)
   		250,   0, 250,
   		250, 250,   0,
   		250, 250, 250;
-		
+
 
     if(debug) std::cout << "\n\ntrackerPoints:\n\n" << trackerPoints << std::endl;
-	
+
    	Eigen::Matrix4d manualTform;
    	manualTform <<
    	1,  0,  0,    1,
    	0,  1,  0,    1,
    	0,  0,  1,    1,
    	0,  0,  0,    1;
-	
-	
+
+
 	Eigen::MatrixXd trackerPointsManTform = trackerPoints;
-	
+
 	trackerPointsManTform.rowwise() += Eigen::Vector3d(1,1,1).transpose();
-    
+
     if(debug) std::cout << "\n\ntrackerPointsManTform:\n\n" << trackerPointsManTform << std::endl;
 
     Eigen::Matrix4d hornTform = hornRegistration(trackerPoints,trackerPointsManTform);
-	 
+
     BOOST_CHECK(manualTform.isApprox(hornTform,tolerance));
     if(debug) PrintTwo(manualTform, hornTform,"manualTform:","hornTform:");
-    
+
     checkHornRegistrationInverses()(trackerPoints,trackerPointsManTform,"trackerPoints","trackerPointsManTform");
-	
+
 }
 
 
@@ -243,57 +243,116 @@ BOOST_AUTO_TEST_CASE(manualHornRegistration)
 BOOST_AUTO_TEST_CASE(testDebugData)
 {
     AlgorithmData ad;
-    
+
     // a
     ad = assembleHW1AlgorithmData(relativeDataPath,pa1debuga);
     visitEachTracker(ad.calbody.frames, ad.calreadings.frames, checkHornRegistrationInverses());
     visitSecondTrackerRepeatedly(ad.calbody.frames, ad.calreadings.frames, checkHornRegistrationInverses());
-    
+
     // b
     ad = assembleHW1AlgorithmData(relativeDataPath,pa1debugb);
     visitEachTracker(ad.calbody.frames, ad.calreadings.frames, checkHornRegistrationInverses());
     visitSecondTrackerRepeatedly(ad.calbody.frames, ad.calreadings.frames, checkHornRegistrationInverses());
-    
+
     // c
     ad = assembleHW1AlgorithmData(relativeDataPath,pa1debugc);
     visitEachTracker(ad.calbody.frames, ad.calreadings.frames, checkHornRegistrationInverses());
     visitSecondTrackerRepeatedly(ad.calbody.frames, ad.calreadings.frames, checkHornRegistrationInverses());
-    
+
     // d
     ad = assembleHW1AlgorithmData(relativeDataPath,pa1debugd);
     visitEachTracker(ad.calbody.frames, ad.calreadings.frames, checkHornRegistrationInverses());
     visitSecondTrackerRepeatedly(ad.calbody.frames, ad.calreadings.frames, checkHornRegistrationInverses());
-    
+
     // e
     ad = assembleHW1AlgorithmData(relativeDataPath,pa1debuge);
     visitEachTracker(ad.calbody.frames, ad.calreadings.frames, checkHornRegistrationInverses());
     visitSecondTrackerRepeatedly(ad.calbody.frames, ad.calreadings.frames, checkHornRegistrationInverses());
-    
+
     // f
     ad = assembleHW1AlgorithmData(relativeDataPath,pa1debugf);
     visitEachTracker(ad.calbody.frames, ad.calreadings.frames, checkHornRegistrationInverses());
     visitSecondTrackerRepeatedly(ad.calbody.frames, ad.calreadings.frames, checkHornRegistrationInverses());
-    
+
     // g
     ad = assembleHW1AlgorithmData(relativeDataPath,pa1debugg);
     visitEachTracker(ad.calbody.frames, ad.calreadings.frames, checkHornRegistrationInverses());
     visitSecondTrackerRepeatedly(ad.calbody.frames, ad.calreadings.frames, checkHornRegistrationInverses());
 }
 
+BOOST_AUTO_TEST_CASE(PivotCalibration){
+
+    csvCIS_pointCloudData::TrackerDevices Clouds;
+
+    Eigen::MatrixXd probePointsTest(5,3);
+	probePointsTest <<
+  		  0,   1,   0,
+  		  1,   2,   0,
+         -1,   2,   0,
+  		  2,   3,   0,
+  		 -2,   3,   0;
+
+    Clouds.push_back(probePointsTest);
+
+	probePointsTest <<
+  		  1,   0,   0,
+  		  2,   1,   0,
+  		  2,  -1,   0,
+  		  3,   2,   0,
+  		  3,  -2,   0;
+
+    Clouds.push_back(probePointsTest);
+
+	probePointsTest <<
+  		  0,  -1,   0,
+  		  1,  -2,   0,
+         -1,  -2,   0,
+  		  2,  -3,   0,
+  		 -2,  -3,   0;
+
+    Clouds.push_back(probePointsTest);
+
+	probePointsTest <<
+         -1,   0,   0,
+  		 -2,   1,   0,
+         -2,  -1,   0,
+         -3,   2,   0,
+         -3,  -2,   0;
+
+    Clouds.push_back(probePointsTest);
+
+    Eigen::VectorXd result = pivotCalibration(trackerIndexedData);
+    Eigen::Vector3d checkResultFirst = result.block<3,1>(0,0);
+    Eigen::Vector3d checkResultSecond = result.block<3,1>(3,0);
+    Eigen::Vector3d checkOutput = ad.output1.frames[0][0].block<1,3>(0,0).transpose();
+
+    BOOST_CHECK(checkResultFirst.isApprox(checkOutput,tolerance));
+
+    if (debug) {
+        std::cout << "\n\nresult:\n\n" << result << "\n\n";
+        std::cout << "\n\ncheckresult FULL:\n\n" << result << "\n\n";
+        std::cout << "\n\ncheckresult FIRST:\n\n" << checkResultFirst << "\n\ncheckresult SECOND:\n\n" << checkResultSecond << "\n\ncheckoutput:\n\n" << checkOutput << "\n\n";
+    }
+
+
+}
+
 void testOnePivotCalibration(std::string relativeDataPath,std::string datapathsuffix){
-    
+
     if (debug) {
         std::cout << "testing " <<relativeDataPath<<datapathsuffix<<"\n";
         std::cout << "===============================================\n\n";
     }
-    
+
+
+
     AlgorithmData ad;
     csvCIS_pointCloudData::TrackerDevices trackerIndexedData;
-    
+
     // Note: we know there is only one tracker in this data
     //       so we can run concat to combine the vectors and
     //       and do the calibration for it.
-    
+
     // a
     ad = assembleHW1AlgorithmData(relativeDataPath,datapathsuffix);
     trackerIndexedData = concat(ad.empivot.frames);
@@ -301,9 +360,9 @@ void testOnePivotCalibration(std::string relativeDataPath,std::string datapathsu
     Eigen::Vector3d checkResultFirst = result.block<3,1>(0,0);
     Eigen::Vector3d checkResultSecond = result.block<3,1>(3,0);
     Eigen::Vector3d checkOutput = ad.output1.frames[0][0].block<1,3>(0,0).transpose();
-    
+
     BOOST_CHECK(checkResultFirst.isApprox(checkOutput,tolerance));
-    
+
     if (debug) {
         std::cout << "\n\nresult:\n\n" << result << "\n\n";
         std::cout << "\n\ncheckresult FULL:\n\n" << result << "\n\n";
@@ -320,7 +379,7 @@ BOOST_AUTO_TEST_CASE(pivotCalibrationTest)
     testOnePivotCalibration(relativeDataPath, pa1debuge);
     testOnePivotCalibration(relativeDataPath, pa1debugf);
     testOnePivotCalibration(relativeDataPath, pa1debugg);
-    
+
     //Print(trackerIndexedData,true,"trackerIndexedData:");
 }
 

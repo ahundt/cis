af9837e3c37ef79b4b00370e5f6740f842953757
Andrew Hundt <ATHundt@gmail.com>
2014-10-29 16:49:50 -0400

    Fixed Bugs in PointEstimation.hpp

--- include/PointEstimation.hpp
+++ include/PointEstimation.hpp
@@ -10,48 +10,59 @@ std::vector<Eigen::MatrixXd> estimateCExpected(const T& calreadings, const T& ca
     
     BOOST_VERIFY(!calreadings.empty() && !calbody.empty());
 
-    std::vector<Eigen::Matrix4d> transformsEMcoordtoTrackerLocation;
-    std::vector<Eigen::Matrix4d> transformsOptcoordtoTrackerLocation;
     // points with dimensions 3xnumTrackers calReadingsActualEMTrackerPositionOnCalibrationObject
-    std::vector<Eigen::MatrixXd> calObjEMTrackerPosV;
+    Eigen::MatrixXd calObjEMTrackerPoints;
+    
+    
+    Eigen::MatrixXd opticalMarkersOnEMDeviceInEMFrame(calbody[0][0]); // A
+    Eigen::MatrixXd OptMarkersOnCalObjInCalObjFrame(calbody[0][1]);   // B
+    Eigen::MatrixXd EMMarkersOnCalObjInCalObjFrame(calbody[0][2]);    // C
+    
+    
+    std::vector<Eigen::MatrixXd> cExpectedV; // expected output
     
     // load up relevant transforms
     for (int i = 0; i< calreadings.size(); ++i){
         BOOST_VERIFY(calreadings[i].size()==3);
         // only copy first two falues in here
         // perform hornRegistration and add to back of vector
-        transformsEMcoordtoTrackerLocation.push_back(homogeneousInverse(hornRegistration(calreadings[i][0],calbody[0][0])));
-        transformsOptcoordtoTrackerLocation.push_back(hornRegistration(calreadings[i][1],calbody[0][1]));
-        calObjEMTrackerPosV.push_back(calreadings[i][2]);
-    }
-
-    if(debug) std::cout << "\n\nsolveForCExpected\n\n" << transformsEMcoordtoTrackerLocation.size() << "\n\n";
-	
-    for (int i = 0; i<10; i++)
-        std::cout << transformsEMcoordtoTrackerLocation[i] << "\n\n";
-    
-    std::vector<Eigen::MatrixXd> cExpectedV;
-    for (int i = 0; i< calreadings.size(); ++i){
-        Eigen::Affine3d transformEM;
-        transformEM.setIdentity();
-        transformEM.matrix() = transformsEMcoordtoTrackerLocation[i].block<4,4>(0,0);
         
-        Eigen::Affine3d transformOpt;
-        transformOpt.setIdentity();
-        transformOpt.matrix() = transformsOptcoordtoTrackerLocation[i].block<4,4>(0,0);
+        // input points are in EM Tracker frame
+        // output points are in Optical Tracker Frame
+        // Find transform from the calibration object reference frame to the EM coordinates of the calibration object
+        Eigen::MatrixXd OptMarkersOnEMDeviceInOptFrame(calreadings[i][0]); // D
+        Eigen::Affine3d OptToEMFrame(hornRegistration( // horn(D,A)
+                                                      OptMarkersOnEMDeviceInOptFrame,
+                                                      opticalMarkersOnEMDeviceInEMFrame
+                                                      ));
         
-        Eigen::MatrixXd calObjEMTrackerPos(calObjEMTrackerPosV[i].rows(),3);
-        for(std::size_t j = 0; j < calObjEMTrackerPosV[i].rows(); ++j){
+        // input points are in calibration object frame
+        // output points are in optical tracker frame
+        // Find transform from the calibration object reference frame to the EM coordinates of the calibration object
+        Eigen::MatrixXd OptMarkersOnCalObjectInOptFrame(calreadings[i][1]); // E
+        Eigen::Affine3d CalObjToOptFrame(hornRegistration( // horn(B,E)
+                                                          OptMarkersOnCalObjInCalObjFrame,
+                                                          OptMarkersOnCalObjectInOptFrame
+                                                          ));
+        
+        
+        Eigen::MatrixXd calObjPtsInEMTrackerF(EMMarkersOnCalObjInCalObjFrame.rows(),3);
+        for(std::size_t j = 0; j < EMMarkersOnCalObjInCalObjFrame.rows(); ++j){
             //  extract calibration Object EM Tracker Positions On Calibration Object into individual points
-            Eigen::Vector3d calObjEMTrackerPosInCalibObjFrame(calObjEMTrackerPosV[i].row(j).transpose());
-            Eigen::Vector3d calObjEMTrackerPosInEMFrame = transformEM*transformOpt*calObjEMTrackerPosInCalibObjFrame;
+            Eigen::Vector3d EMMarkerOnCalObjInCalObjFrame(EMMarkersOnCalObjInCalObjFrame.row(j).transpose()); // Extract point from C
+            
+            // starting with point on calibration object in calObj frame -> optFrame -> EMFrame
+            Eigen::Vector3d calObjEMTrackerPosInEMFrame = OptToEMFrame*CalObjToOptFrame*EMMarkerOnCalObjInCalObjFrame;
+            
             // would be .row(j) but it is MatrixXd so we need to use .block to specify the size
             // note that the output of the transform multiplication is already in row format
             // so it can be inserted directly
-            calObjEMTrackerPos.block<1,3>(j,0) = calObjEMTrackerPosInEMFrame.transpose();
+            calObjPtsInEMTrackerF.block<1,3>(j,0) = calObjEMTrackerPosInEMFrame.transpose();
         }
-        cExpectedV.push_back(calObjEMTrackerPos);
+        
+        cExpectedV.push_back(calObjPtsInEMTrackerF);
     }
+
 	
 	return cExpectedV;
 }

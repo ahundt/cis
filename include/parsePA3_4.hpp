335a235f0ba817db4ce09f7d026d342f61856f3e
Andrew Hundt <ATHundt@gmail.com>
2014-11-17 02:22:11 -0500

    implemented ParseSampleBody

--- include/parsePA3_4.hpp
+++ include/parsePA3_4.hpp
@@ -13,6 +13,13 @@ struct problemBody {
 	Eigen::Vector3d tip;
 };
 
+struct sampleBody {
+	std::string title;
+	std::vector<std::vector<Eigen::Vector3d> > NA;
+	std::vector<std::vector<Eigen::Vector3d> > NB;
+	std::vector<std::vector<Eigen::Vector3d> > ND;
+};
+
 cisMesh parseMesh(std::string csv, bool debug = false){
     cisMesh outputData;
     if(csv.empty()) return outputData;
@@ -87,6 +94,70 @@ problemBody parseProblemBody(std::string csv, bool debug = false){
 	}
 	
 	outputData.tip = Eigen::Vector3d(readPointString(*currentStringLineIterator));
+	
+	return outputData;
+}
+
+template<typename T>
+std::vector<Eigen::Vector3d> readNXrecords(T& iterator, T& end, int NS){
+    std::vector<Eigen::Vector3d> sample;
+	Eigen::Vector3d zero(0.0,0.0,0.0);
+	bool previousWasZero = false;
+	bool currentIsZero = false;
+	
+	// read NA records
+	for(int i = 0; (i < NS) && iterator != end; ++i, ++currentStringLineIterator){
+		Eigen::Vector3d point(readPointString(*currentStringLineIterator));
+		currentIsZero = (point == zero);
+		
+		if(point != zero && previousWasZero) break; // stop iterating this loop when you hit the last zero points
+		else if((point != zero) && (!currentIsZero)) sample.push_back(point); // don't add zero points to the list
+		
+		previousWasZero = currentIsZero;
+	}
+	
+	return sample;
+}
+
+
+problemBody parseSampleBody(std::string csv, bool debug = false){
+    sampleBody outputData;
+	
+    if(csv.empty()) return outputData;
+       
+    std::vector<std::string> strs;
+    
+    // by default if no frame count is specified, there is only one frame
+    int numFrames = 1;
+    // split into separate strings by newlines
+	boost::split(strs, csv, boost::is_any_of("\n"));
+    
+    if(debug) {
+        //std::cout << "\n\n FULL FILE CONTENTS:\n\n" << csv;  // original file string
+        std::cout << "\n\nNewly split vector of strings:\n\n"; // file string post split on newlines
+        printStringVector(strs);
+    }
+    
+    
+	// parse first line differently
+    std::vector<std::string>::iterator currentStringLineIterator = strs.begin();
+	std::vector<std::string> firstLineStrings;
+	
+	boost::split(firstLineStrings, *currentStringLineIterator, boost::is_any_of(", "), boost::token_compress_on);
+	int NS =  boost::lexical_cast<int>(firstLineStrings[0]);
+	int nSamps =  boost::lexical_cast<int>(firstLineStrings[1]);
+	
+	++currentStringLineIterator;
+	
+	for(int j = 0; j < nSamps; ++j){
+		// read NA records
+		outputData.NA.push_back(readNXrecords(currentStringLineIterator,strs.end(),NS));
+		// read NB records
+		outputData.NB.push_back(readNXrecords(currentStringLineIterator,strs.end(),NS));
+		// read ND records
+		outputData.ND.push_back(readNXrecords(currentStringLineIterator,strs.end(),NS));
+	
+    }
 	
 	return outputData;
 }

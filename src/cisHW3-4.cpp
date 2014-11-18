554daf3486b461e1c7dc11cb0a27175ac1ff170d
Andrew Hundt <ATHundt@gmail.com>
2014-11-18 04:17:56 -0500

    extracted and documented icpPointMeshRegistration

--- src/cisHW3-4.cpp
+++ src/cisHW3-4.cpp
@@ -216,32 +216,16 @@ bool readCommandLine(int argc, char* argv[], ParsedCommandLineCommandsPA3_4 & pc
     return false;
 }
 
+
+/// run the PA3 algorithm and create then write the output file
 void generateOutputFilePA3_4(AlgorithmDataPA3_4 ad, std::string outputDataFolderPath, std::string dataFilenamePrefix, bool debug = false){
 	
-	Eigen::Vector3d ckMin;
-	std::vector<Eigen::Vector3d> dk;
-	std::vector<Eigen::Vector3d> ck;
-	std::vector<double> errork;
-	Eigen::Affine3d Freg;
-    Freg.setIdentity();
     
-    for (int i=0; i<ad.sampleReadings.NA.size(); i++){
-        double errorMin=std::numeric_limits<double>::max();
-		Eigen::Affine3d FaAffine(hornRegistration(ad.bodyA.markerLEDs,ad.sampleReadings.NA[i])); // a: PA3-A-Debug-SampleReadingsTest A: Problem3-BodyA
-		Eigen::Affine3d FbInverseAffine(hornRegistration(ad.sampleReadings.NB[i], ad.bodyB.markerLEDs)); // b: PA3-A-Debug-SampleReadingsTest B: Problem3-BodyB
-        dk.push_back(Eigen::Vector3d(FbInverseAffine*FaAffine*ad.bodyA.tip)); // Atip: Problem3-BodyA (last line)
-		Eigen::Vector3d sk = Freg*dk[i];
-        for (auto&& triangle : ad.mesh.vertexTriangleNeighborIndex){
-			Eigen::Vector3d ckTemp = FindClosestPoint(sk, ad.mesh.vertices, triangle);
-			double errorTemp = (ckTemp-dk[i]).norm();
-			if (errorTemp < errorMin){
-				ckMin = ckTemp;
-				errorMin = errorTemp;
-			}
-		}
-		ck.push_back(ckMin);
-		errork.push_back(errorMin);
-	}
+    std::vector<Eigen::Vector3d> dk;
+    std::vector<Eigen::Vector3d> ck;
+    std::vector<double> errork;
+    
+    icpPointMeshRegistration(ad.sampleReadings.NA, ad.sampleReadings.NB, ad.bodyA.tip, ad.bodyA.markerLEDs, ad.bodyB.markerLEDs, ad.mesh.vertices, ad.mesh.vertexTriangleNeighborIndex,dk,ck,errork);
     
     std::string outputFilename =  dataFilenamePrefix + "-Output.txt";
     std::ofstream ofs (outputFilename, std::ofstream::out);
@@ -250,6 +234,8 @@ void generateOutputFilePA3_4(AlgorithmDataPA3_4 ad, std::string outputDataFolder
     ofs.close();
     
 }
+
+
 
 /**************************************************************************/
 /**

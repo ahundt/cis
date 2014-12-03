e8035990ff1a772a6a801a02a0b3ab640bb09ef1
Andrew Hundt <ATHundt@gmail.com>
2014-12-02 21:21:00 -0500

    Documented TerminationCriteria.hpp

--- include/TerminationCriteria.hpp
+++ include/TerminationCriteria.hpp
@@ -17,6 +17,9 @@
 
 namespace acc = boost::accumulators;
 
+/// parameters for the TerminationCriteria object
+/// this struct is useful to facilitate reading
+/// parameters from the command line or other sources
 struct TerminationCriteriaParams {
     TerminationCriteriaParams():
         meanErrorThreshold(0.01),
@@ -26,14 +29,29 @@ struct TerminationCriteriaParams {
         minIterationCount(10),
         trackProgressEveryNIterations(10){}
     
+    /// Stop the algorithm once the mean error is *below* this amount
     double meanErrorThreshold;
+    
+    /// Stop the algorithm once the max error is *below* this amount
     double maxErrorThreshold;
+    
+    /// The mean error is calculated on each iteration.
+    /// Over a series of iterations, a rolling variance is calculated
+    /// for the mean error. If the variance becomes too small, this means
+    /// that the mean is no longer improving, so the algorithm should stop.
+    ///
+    /// Typically use a very small value like 1e-10
     double minVarianceInMeanErrorBetweenIterations;
-    int    maxCount;
+    
+    /// the maximum number of iterations to run
     int    maxIterationCount;
+    
+    /// the minimum number of iterations to run
     int    minIterationCount;
     
-    int    trackProgressEveryNIterations; // 0 means don't track progress
+    /// Print out the progress and stats every Nth iteration.
+    /// 0 means don't track progress.
+    int    trackProgressEveryNIterations;
     
 };
 
@@ -42,15 +60,20 @@ struct TerminationCriteriaParams {
 struct TerminationCriteria {
     typedef acc::accumulator_set< double, acc::features< acc::tag::min, acc::tag::max, acc::tag::mean, acc::tag::variance, acc::tag::count, acc::tag::rolling_mean, acc::tag::rolling_variance > > accumulator_type;
     
+    /// constructor
     TerminationCriteria(TerminationCriteriaParams tcp = TerminationCriteriaParams()):
         m_tcp(tcp),
         m_acc(new accumulator_type(acc::tag::rolling_window::window_size = m_tcp.minIterationCount)),
         m_iterationMeanAcc(new accumulator_type(acc::tag::rolling_window::window_size = m_tcp.minIterationCount)){}
     
-    void operator()(double i){
-        (*m_acc)(i);
+    /// operator() add a new error data point
+    ///
+    /// @param error the amount of error found in the data point
+    void operator()(double error){
+        (*m_acc)(error);
     }
     
+    /// Returns true if the termination criteria has been met
     bool shouldTerminate(){
         // should have run at least once, plus both the mean and max error should be below a threshold
         bool shouldTerminate_b =
@@ -74,6 +97,7 @@ struct TerminationCriteria {
         return shouldTerminate_b;
     }
 	
+    /// prints various stats about the collected error data for the most recent iteration
 	void PrintIterationStats(){
         if(m_tcp.trackProgressEveryNIterations) {
             std::cout << "\n"
